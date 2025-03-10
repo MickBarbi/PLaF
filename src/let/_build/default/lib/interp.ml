a93002f491b3bc0388b9ffbb0110a482f841c4ba
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2025-03-09 20:17:25 -0400

    implemented binary tree stuff

--- src/let/_build/default/lib/interp.ml
+++ src/let/_build/default/lib/interp.ml
@@ -1,7 +1,7 @@
 open Parser_plaf.Ast
 open Parser_plaf.Parser
 open Ds
-    
+
 (** [eval_expr e] evaluates expression [e] *)
 let rec eval_expr : expr -> exp_val ea_result =
   fun e ->
@@ -54,6 +54,29 @@ let rec eval_expr : expr -> exp_val ea_result =
     string_of_env >>= fun str ->
     print_endline str; 
     error "Debug called"
+  | IsEmpty ( e ) ->
+    eval_expr e >>= fun ev ->
+    (match ev with
+    | TreeVal Empty -> return (BoolVal True)
+    | TreeVal _ -> return (BoolVal False)
+    | _ -> Error "Improper input, IsEmpty requires a tree")
+  | EmptyTree ( _t ) ->
+    return (TreeVal Empty)
+  | Node ( e1 , e2 , e3 ) ->
+    eval_expr e1 >>= fun n1 ->
+    eval_expr e2 >>= fun n2 ->
+    eval_expr e3 >>= fun n3 ->
+    (match (n2, n3) with
+    | (TreeVal t1, TreeVal t2) -> return (TreeVal (Node(n1, n2, n3)))
+    | _ -> Error "2nd and 3rd arguments must be value tree values, Empty or a tree")
+  | CaseT ( e1 , e2 , id1 , id2 , id3 , e3 ) ->
+    eval_expr e1 >>= fun ev ->
+    (match ev with
+    | TreeVal Empty -> eval_expr e2
+    | TreeVal (Node(v, left, right)) ->
+      extend_env_list [id1; id2; id3] [v; TreeVal left; TreeVal right] >>+
+      eval_expr e3
+    | _ -> error "Expected a tree!")
   | _ -> failwith "Not implemented yet!"
 
 (** [eval_prog e] evaluates program [e] *)
